065ecfd515fc4a1e7b693c6e14b2491ff759eea6
Dell <Dell@SURABHI>
2025-02-13 20:19:45 +0400

    my first commit

---
+++ seleniumproject/src/main/java/print/seleniumproject/sample.java
@@ -0,0 +1,16 @@
+package print.seleniumproject;
+
+public class sample {
+	
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello");
+		
+		
+	
+
+	}
+
+}
